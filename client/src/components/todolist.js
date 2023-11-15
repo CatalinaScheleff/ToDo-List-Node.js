d840e4cc5096626e0f3264394eaddbad78c551dd
CatalinaScheleff <catascheleff@gmail.com>
2023-11-14 23:30:14 -0300

    Update todolist.js

--- client/src/components/todolist.js
+++ client/src/components/todolist.js
@@ -4,11 +4,11 @@ import React from 'react';
 const TodoList = () => {
 
     return ( 
-    
+    <div className='justify-content-center d-flex'>
       <div className="col-6">
-    <label for="inputTask" class="form-label">Add a Task:</label>
-    <input type="text" class="form-control" id="inputTask"/>
-  
+      <h1 className='mt-5'>Add a Task</h1>
+    <input type="text" className="form-control my-4"/>
+    <h1 className='mt-5'> Your Tasks</h1>
       <ul className="list-group">
       <li className="list-group-item">An item</li>
       <li className="list-group-item">A second item</li>
@@ -16,6 +16,7 @@ const TodoList = () => {
       <li className="list-group-item">A fourth item</li>
       <li className="list-group-item">And a fifth one</li>
     </ul>
+    </div>
     </div>
     )
 }
